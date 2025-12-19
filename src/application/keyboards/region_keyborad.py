b6b378870130c3161b77518c571dc73ea5d3e010
mrdragar <yshhenyaev@mail.ru>
2025-12-19 18:22:58 +0300

    Fix regions

--- src/application/keyboards/region_keyborad.py
+++ src/application/keyboards/region_keyborad.py
@@ -6,7 +6,7 @@ from src.application.callbacks import RegionCallback, RetryRegionCallback
 def get_region_keyboard(regions: list[str]) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardBuilder()
     for region in regions:
-        keyboard.button(text=region, callback_data=RegionCallback(region=region).pack())
+        keyboard.button(text=region, callback_data=RegionCallback(region=region[:min(30, len(region))]).pack())
     keyboard.button(text="Ввести регион заново", callback_data=RetryRegionCallback().pack())
     keyboard.adjust(1, repeat=True)
     return keyboard.as_markup()
